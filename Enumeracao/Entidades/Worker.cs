2ca0c1fcbf4c88b15e0ad472b0e5054034393689
AndreMilla <andremilla.am@gmail.com>
2020-01-21 23:16:46 -0300

    Program CS 2

--- Enumeracao/Entidades/Worker.cs
+++ Enumeracao/Entidades/Worker.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Enumeracao.Entidades.Enum;
-using System.Collections.Generic;
 
 namespace Enumeracao.Entidades
 {
